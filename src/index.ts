3008ef922d5a687e8c766d8c471c4afb50b4a2da
Shimizu Naruaki <narsw.mail@gmail.com>
2018-06-03 02:35:29 +0900

    初期設定完了

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { SheetService } from './sheet.service';
-
-declare var global: any;
-
-global.createNewFile = (): void => {
-  const ss = SheetService.createInitialFile('New file');
-  ss.getRange('A2').setValue('Happy gas!');
-};
+Logger.log('TEST');
